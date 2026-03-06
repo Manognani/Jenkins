68634c8174cc1cd615077a001b02c2f7578f113f
manognani <manognanigandham@gmail.com>
2026-03-06 11:29:13 +0530

    jenkins commit

---
+++ src/main/java/com/pipeline/jenkins/JenkinsController.java
@@ -0,0 +1,20 @@
+package com.pipeline.jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("jenkins")
+public class JenkinsController {
+
+    @GetMapping("welcome")
+    public String welcome(){
+        return "Welcome";
+    }
+
+    @GetMapping("hello")
+    public String hello(){
+        return "Hello";
+    }
+}
